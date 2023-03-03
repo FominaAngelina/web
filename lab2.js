3a1c1eea0542d962f0d07a3bc1999ace396b4c44
FominaAngelina <114000211+FominaAngelina@users.noreply.github.com>
2023-03-03 16:15:49 +0300

    Add files via upload

---
+++ lab2.js
@@ -0,0 +1,84 @@
+console.log("Задача №1");
+
+function convertSpeed(){
+let speed = +prompt("Введите скорость для конвертации");
+
+while (isNaN(speed) || speed < 0){
+speed = +prompt("Введите положительную скорость");
+}
+
+let trand = prompt("Введите конвертацию в м/с 'toMS' или в км/ч 'toKMH");
+
+while (true) {
+    if(trand === "toMS" || trand === "toKMH") break;
+    trand = prompt("Введите конвертацию в м/с 'toMS' или в км/ч 'toKMH'");
+}
+
+if(trand === 'toKMH') console.log(`convertSpeed(${speed}, '${trand}') -> '${speed*3600/1000}`);
+else console.log(`convertSpeed(${speed}, '${trand}') -> '${speed*1000/3600}`);
+}
+
+convertSpeed();
+
+console.log("\nЗадача №2");
+
+function absValue(number){
+if(number > 0 || number === 0) return number;
+else return number*(-1);
+}
+
+let number = +prompt("Введите число");
+
+while (isNaN(number)){
+number = +prompt("Введите число");
+}
+
+console.log(`absValue(${number}) -> ${absValue(number)}`);
+
+console.log("\nЗадача №3");
+
+let student = {
+group: 326,
+surname: "Фомина",
+name: "Ангелина"
+};
+
+console.log(`Список свойств: ${Object.keys(student)}`);
+
+console.log(`Cтудент ${student.name} ${student.surname} учится в ${student.group} группе`);
+
+
+console.log("\nЗадача №4");
+
+
+function randomNumder(min, max) {
+    if(min >= 0){
+        const num = Math.floor(Math.random() * (max - min + 1)) + min;
+        return num;
+    }
+    else if (min < 0 && max >= 0){
+        const num = Math.floor(Math.random() * (max + min + 1));
+        return num;
+    }
+    else{
+        const num = Math.floor(Math.random() * ((-max + min)*(-1) + 1)) + min;
+        return num;
+    }
+}
+
+let min = +prompt("Введите минимальное число.");
+let max = +prompt("Введите максимальное число");
+
+console.log(`randomNumber (${min}, ${max}) -> ${randomNumder(min, max)}`);
+
+console.log("\nЗадача №5");
+
+const sampleArray = [58, 2, 5,54,3,8,1,7,89,2,7,9,44,56,4,58,44];
+let leng = sampleArray.length;
+
+let kolvo = +prompt("Введите длину нового массива");
+const newMas = [];
+
+for (let i = 0; i < kolvo; i++) newMas.push(sampleArray[randomNumder(0, leng)]);
+
+console.log(`sampleArray([${sampleArray}], ${kolvo}) -> [${newMas}]`);
